f8f8d93ea297cf45d9f347a6aeb569e37fb01bea
ybhav <ybhav@infocrunch.in>
2019-09-30 13:35:18 +0530

    Removed List import

--- eventtracker/src/main/java/com/info/modules/admin/controller/AdminController.java
+++ eventtracker/src/main/java/com/info/modules/admin/controller/AdminController.java
@@ -14,7 +14,6 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.mail.MessagingException;
 import java.io.UnsupportedEncodingException;
-import java.util.List;
 
 @RestController
 @RequestMapping("mvc")
